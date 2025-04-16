08b203a2457ccd3a640b1f95260e7965a76bc0b3
St0ny <42348051+Raz0r1337@users.noreply.github.com>
2025-04-16 13:54:09 +0200

    Add spawn.sql
    Addes spawn.sql to spawn the NPCs in Dalaran.

---
+++ spawn.sql
@@ -0,0 +1,44 @@
+-- GreedyJoe
+DELETE FROM `creature` WHERE `id1` = 999001;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999001, 571, 4395, 4564, 1, 1, 5846.322, 492.46835, 657.23676, 3.4360633, 300, 0, 0, 'GreedyJoe');
+
+-- CunningJim
+DELETE FROM `creature` WHERE `id1` = 999002;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999002, 571, 4395, 4564, 1, 1, 5750.2583, 592.8158, 613.7277, 1.2275507, 300, 0, 0, 'CunningJim');
+
+-- TrickyJohn
+DELETE FROM `creature` WHERE `id1` = 999003;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999003, 571, 4395, 4564, 1, 1, 5750.24, 737.133, 653.661, 0.753139, 300, 0, 0, 'TrickyJohn');
+
+-- GreenJack
+DELETE FROM `creature` WHERE `id1` = 999004;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999004, 571, 4395, 4564, 1, 1, 5876.87, 702.11, 644.691, 4.75081, 300, 0, 0, 'GreenJack');
+
+-- VelvetyJosh
+DELETE FROM `creature` WHERE `id1` = 999005;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999005, 571, 4395, 4564, 1, 1, 5878.587, 745.63153, 640.37177, 0.87882775, 300, 0, 0, 'VelvetyJosh');
+
+-- CreapyJaden
+DELETE FROM `creature` WHERE `id1` = 999006;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999006, 571, 4395, 4564, 1, 1, 5911.2695, 755.8454, 641.0746, 2.9632776, 300, 0, 0, 'CreapyJaden');
+
+-- StoneyJamal
+DELETE FROM `creature` WHERE `id1` = 999007;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999007, 571, 4395, 4564, 1, 1, 5907.9517, 685.53375, 643.4958, 5.4490557, 300, 0, 0, 'StoneyJamal');
+
+-- GlitteryJason
+DELETE FROM `creature` WHERE `id1` = 999008;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999008, 571, 4395, 4564, 1, 1, 5872.31, 721.557, 643.089, 6.10487, 300, 0, 0, 'GlitteryJason');
+
+-- EternalJerrik
+DELETE FROM `creature` WHERE `id1` = 999009;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (999009, 571, 4395, 4564, 1, 1, 5906.62, 684.29, 643.496, 5.51029, 300, 0, 0, 'EternalJerrik');
